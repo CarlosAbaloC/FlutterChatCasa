9fde239cb38d95dddab2da285efcf0dbd0041a0b
CarlosAbaloC <carlos.abalocordero@gmail.com>
2023-01-11 20:28:08 +0100

    HomeView + images

--- lib/src/home_views/HomeView.dart
+++ lib/src/home_views/HomeView.dart
@@ -9,6 +9,7 @@ import 'package:flutter_chat_casa/src/singleton/DataHolder.dart';
 import '../fb_objects/Perfil.dart';
 import '../fb_objects/Room.dart';
 import '../list_items/RoomItem.dart';
+import 'package:http/http.dart' as http;
 
 class HomeView extends StatefulWidget{
 
@@ -31,6 +32,44 @@ class _HomeViewState extends State<HomeView> {
   void initState() {
     super.initState();
     actualizarLista();
+  }
+
+  void getPilotosF1() async{
+
+
+
+    final response = await http
+        .get(Uri.parse('http://ergast.com/api/f1/2022/drivers.json?callback=myParser'));
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      print("DEBUG: --->>>>>>>>>   "+response.body);
+      //return Album.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
+  void getRoomsList() async{
+    //String Query=SELECT * FROM ROOMS WHERE MEMBERS >25 && MEMBERS <100
+    //final docsRef = db.collection("rooms").where("members",isGreaterThan: 25,isLessThan: 100).
+    final docsRef = db.collection("rooms").where("members",isGreaterThan: 25,isLessThan: 170).
+    orderBy("members",descending: false).
+    withConverter(fromFirestore: Room.fromFirestore,
+        toFirestore: (Room room, _) => room.toFirestore());
+
+    //stmt.executeQuery(Query);
+    final docsSnap = await docsRef.get();
+
+    setState(() {
+      for(int i=0;i<docsSnap.docs.length;i++){
+        chatRooms.add(docsSnap.docs[i].data());
+      }
+    });
+
   }
 
 
